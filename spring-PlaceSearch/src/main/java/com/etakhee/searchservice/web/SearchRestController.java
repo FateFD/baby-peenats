d3a087d830f47b2e264f43f49d2fb3c65479ea6a
이탁희 <etakhee>
2019-07-01 17:57:59 +0900

    테스트 코드 삭제

--- spring-PlaceSearch/src/main/java/com/etakhee/searchservice/web/SearchRestController.java
+++ spring-PlaceSearch/src/main/java/com/etakhee/searchservice/web/SearchRestController.java
@@ -20,16 +20,13 @@ public class SearchRestController {
     		RestTemplate restTemplate = new RestTemplate(); 
         	
         	HttpHeaders headers = new HttpHeaders(); 
-        	headers.setContentType(MediaType.APPLICATION_JSON);//JSON
+        	headers.setContentType(MediaType.APPLICATION_JSON);
         	headers.set("Authorization", "KakaoAK" + " " + "a263ae2bb6f386cafb5b3c40803ca359");
         	
         	HttpEntity entity = new HttpEntity("parameters", headers); 
         	URI url=URI.create("https://dapi.kakao.com/v2/local/search/address.json?query=" + query); 
         	
         	ResponseEntity response= restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-        	
-        	System.out.println(response);
-
     	}catch(Exception ex) {
     		System.err.println(ex);
     	}
