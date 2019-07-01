21f0649ccafb4bc76b90e99560a3e2d7138a88d5
이탁희 <etakhee>
2019-07-01 17:51:18 +0900

    장소찾기 결과값 호출까지 구현 TODO:파싱해서 리스트로 표현

--- spring-PlaceSearch/src/main/java/com/etakhee/searchservice/web/SearchRestController.java
+++ spring-PlaceSearch/src/main/java/com/etakhee/searchservice/web/SearchRestController.java
@@ -1,17 +1,38 @@
 package com.etakhee.searchservice.web;
 
+import java.net.URI;
+
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
-import com.etakhee.searchservice.domain.accounts.*;
+import org.springframework.web.client.RestTemplate;
 
 import lombok.*;
 
 @RestController
 @AllArgsConstructor
 public class SearchRestController {
-    private AccountsRepository accountRepository;
-    
-    @PostMapping("/accounts")
-    public void createAccount(@RequestBody AccountsSaveRequestDto dto) {
-        accountRepository.save(dto.toEntity());
+
+    @GetMapping("/kakaomap_search")
+    @ResponseBody
+    public String search_map(@RequestParam String query) {
+    	
+    	try {
+    		RestTemplate restTemplate = new RestTemplate(); 
+        	
+        	HttpHeaders headers = new HttpHeaders(); 
+        	headers.setContentType(MediaType.APPLICATION_JSON);//JSON
+        	headers.set("Authorization", "KakaoAK" + " " + "a263ae2bb6f386cafb5b3c40803ca359");
+        	
+        	HttpEntity entity = new HttpEntity("parameters", headers); 
+        	URI url=URI.create("https://dapi.kakao.com/v2/local/search/address.json?query=" + query); 
+        	
+        	ResponseEntity response= restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        	
+        	System.out.println(response);
+
+    	}catch(Exception ex) {
+    		System.err.println(ex);
+    	}
+    	return "111";
     }
 }
